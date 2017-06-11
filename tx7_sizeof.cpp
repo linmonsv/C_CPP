f88083305f71aa50de0a242206a5d5a682d262d1
qin2 <qin2@qq.com>
2017-06-11 19:47:42 +0800

    rerult 8

---
+++ tx7_sizeof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Class_sample {
+    int a, b;
+    int func();
+}Class_a;
+
+int main()
+{
+    cout << sizeof(Class_a) << endl;
+    return 0;
+}
