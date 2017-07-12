5dc8fa69f21b5a03884283df5a8e4ca645af0d62
qin2 <qin2@qq.com>
2017-07-12 11:47:12 +0800

    add istream_iterator

--- Professional C++/t_20_2_2_ostream_iterator.cpp
+++ Professional C++/t_20_2_2_ostream_iterator.cpp
@@ -11,5 +11,10 @@ int main()
 	iota(begin(myVector), end(myVector), 1);
 	copy(cbegin(myVector), cend(myVector), ostream_iterator<int>(cout, " "));
 
+	istream_iterator<int> numbersIter(cin);
+	istream_iterator<int> endIter;
+	int sum = accumulate(numbersIter, endIter, 0);
+	cout << "Sum: " << sum << endl;
+
 	return 0;
 }
